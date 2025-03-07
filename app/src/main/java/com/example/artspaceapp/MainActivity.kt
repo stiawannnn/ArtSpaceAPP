7e59320cf7e9f4bb314b2003b3e71279f9c2d33c
STIAWAN . 4 <darkmx123123@gmail.com>
2025-03-07 13:19:39 +0700

    commit

--- app/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -7,7 +7,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Button
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.material.ButtonDefaults
+//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -22,8 +27,6 @@ import androidx.compose.ui.graphics.Color
 
 data class Artwork(
     val title: String,
-    val author: String,
-    val year: String,
     val imageRes: Int
 )
 
@@ -39,9 +42,27 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ArtSpaceApp() {
     val artworks = listOf(
-        Artwork("The Storm on the Sea of Galilee", "Rembrandt van Rijn", "1633", R.drawable.thestrom),
-        Artwork("Mona Lisa", "Leonardo Da Vinci", "1503", R.drawable.monalisa),
-        Artwork("The Scream", "Edvard Munch", "1893", R.drawable.scream)
+        Artwork(
+            "SIMATIF 2023",
+            R.drawable.kegiatan1
+
+        ),
+        Artwork
+            (
+            "Seminar English Communication Skill",
+            R.drawable.kegiatan2),
+        Artwork
+            ("PBAK Fakultas 2023",
+            R.drawable.kegiatan3),
+
+        Artwork(
+            "Kegiatan Milad TIF-24",
+            R.drawable.kegiatan4),
+
+        Artwork(
+            "Acara Bakar Ayam Sekelas",
+            R.drawable.kegiatan5
+        ),
     )
 
     var currentIndex by remember { mutableIntStateOf(0) }
@@ -56,30 +77,47 @@ fun ArtSpaceApp() {
             }
         }
     }
+    val backgroundColor = Color(0xFFE0E0E0)
+
 
     Box(
-        modifier = Modifier.fillMaxSize().then(swipeGesture)
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundColor).padding(3.dp)
+            .then(swipeGesture)
     ) {
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Text(
+                text = "Daftar 5 Foto Kegiatan",
+                fontSize = 38.sp,
+                fontStyle = androidx.compose.ui.text.font.FontStyle.Italic,
+                color = Color.Black,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 14.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
             Image(
                 painter = painterResource(id = artworks[currentIndex].imageRes),
                 contentDescription = "Artwork",
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(500.dp)
+                    .height(280.dp)
             )
 
             Spacer(modifier = Modifier.height(30.dp))
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color.LightGray)
-                    .padding(18.dp),
-                        contentAlignment = Alignment.Center
+                    .background(Color(0xFF5A5A5A), shape = RoundedCornerShape(400.dp))
+                    .padding(9.dp),
+                contentAlignment = Alignment.Center
             ) {
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally
@@ -87,17 +125,7 @@ fun ArtSpaceApp() {
                     Text(
                         text = artworks[currentIndex].title,
                         fontSize = 24.sp,
-                        textAlign = TextAlign.Center
-                    )
-                    Text(
-                        text = "By ${artworks[currentIndex].author}",
-                        fontSize = 18.sp,
-                        textAlign = TextAlign.Center
-                    )
-                    Text(
-                        text = "Year: ${artworks[currentIndex].year}",
-                        fontSize = 18.sp,
-                        textAlign = TextAlign.Center
+                        textAlign = TextAlign.Center ,color = Color.White
                     )
 
                 }
@@ -111,12 +139,21 @@ fun ArtSpaceApp() {
                 .padding(16.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Button(onClick = { if (currentIndex > 0) currentIndex-- }) {
-                Text("Previous")
+            Button(
+                onClick = { if (currentIndex > 0) currentIndex-- },
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF5A5A5A))
+            ) {
+                Text("Previous", color = Color.White)
             }
-            Button(onClick = { if (currentIndex < artworks.size - 1) currentIndex++ }) {
-                Text("Next")
+
+            Button(
+                onClick = { if (currentIndex < artworks.size - 1) currentIndex++ },
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF5A5A5A))
+
+            ) {
+                Text("Next", color = Color.White)
             }
+
         }
     }
 }
